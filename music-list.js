2b777f901d5a39405638b6a3c110e31de1778e5d
Syam Adam <88608099+syam-adam@users.noreply.github.com>
2022-06-02 07:23:34 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+  {
+    name: "Cheap Thrills",
+    artist: "Sia",
+    img: "Sia",
+    src: "Sia"
+  },
+  {
+    name: "Sakhiyaan",
+    artist: "Maninder Buttar",
+    img: "Sakhiyaan",
+    src: "Sakhiyaan"
+  },
+  {
+    name: "Baby",
+    artist: "Justin Bieber",
+    img: "Justin Bieber - Baby",
+    src: "Justin Bieber - Baby"
+  }
+];
